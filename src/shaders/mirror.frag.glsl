437a5316f50c286ff75a993388d815fc79da154c
Elias Boschung <siela1915@gmail.com>
2023-03-30 14:26:11 +0200

    Add GL3.2.3

--- src/shaders/mirror.frag.glsl
+++ src/shaders/mirror.frag.glsl
@@ -5,8 +5,8 @@ precision mediump float;
 	* surface normal
 	* view vector: direction to camera
 */
-//varying ...
-//varying ...
+varying vec3 v2f_dir_from_camera;
+varying vec3 v2f_normal;
 
 uniform samplerCube cube_env_map;
 
@@ -17,6 +17,12 @@ void main()
 	Calculate the reflected ray direction R and use it to sample the environment map.
 	Pass the resulting color as output.
 	*/
-	vec3 color = vec3(0., 0.5, 0.);
+        vec3 n_v2f_dir_from_camera = normalize(v2f_dir_from_camera);
+        vec3 n_v2f_normal = normalize(v2f_normal);
+        
+        vec3 reflected = reflect(n_v2f_dir_from_camera, n_v2f_normal);
+
+        vec3 color = vec3(textureCube(cube_env_map, reflected));
+        
 	gl_FragColor = vec4(color, 1.); // output: RGBA in 0..1 range
 }
